000a2b195895fa53d78dbe0742e261628df74864
davissen moorgan <davissen@example.com>
2016-06-15 15:25:56 -0400

    Home Work

---
+++ cheat sheet.js
@@ -0,0 +1,30 @@
+//Variable
+Var a = 1; var b = "apple"; var c = document.getElementById("example")
+
+// Debugging (alerts, comments, the console)
+console.log("This is a test");
+debugger;
+alert("Debugger at work")
+
+// Different data types
+numbers = 1, 2, 999, -999
+strings ="Lemon", "Orange"
+Boolean = true, false / var x = true
+
+// Array
+Pizza = ["Papa Johns", "Pizza Hut", "Dominos"]
+Cities = ["Paris", "london", "Toronto"]
+
+// Test
+24 > 56;
+
+// Logic
+if ( 10 < 11){
+	console.log("10 is less than 11")
+}
+
+// Function
+function name(text){
+	console.log(text)
+}
+
